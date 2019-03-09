620ecff9254bf4aababcf0dfc455ba5696a8d2c1
Rakesh Tembhurne <tj2point0@gmail.com>
2019-03-09 10:03:13 +0530

    Added google anayltics code

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-4295037-3',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
